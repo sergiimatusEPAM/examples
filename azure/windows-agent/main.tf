011fff74790c76d25492a85879d90ed529959530
Sergii Matus <sergii_matus@epam.com>
2019-07-31 17:54:20 +0300

    Added Windows module at AzureRM provider

--- azure/windows-agent/main.tf
+++ azure/windows-agent/main.tf
@@ -6,7 +6,14 @@ data "http" "whatismyip" {
 }
 
 locals {
-  cluster_name = "sm-dev1"
+  cluster_name     = "sm-dev1"
+  location         = "North Europe"
+  dcos_version     = "1.13.0"
+  dcos_variant     = "open"
+  dcos_instance_os = "centos_7.6"
+  dcos_winagent_os = "windows_1809"
+  vm_size          = "Standard_D8s_v3"
+  ssh_public_key_file = "~/.ssh/aws-meso-wind.pub"
 }
 
 module "dcos" {
@@ -14,22 +21,22 @@ module "dcos" {
   version = "~> 0.2.0"
 
   providers = {
-    aws = "azure"
+    azure = "azure"
   }
 
-  location = "North Europe"
+  location = "${local.location}"
 
   cluster_name        = "${local.cluster_name}"
-  ssh_public_key_file = "~/.ssh/aws-meso-wind.pub"
+  ssh_public_key_file = "${local.ssh_public_key_file}"
   admin_ips           = ["${data.http.whatismyip.body}/32"]
 
   num_masters        = "1"
   num_private_agents = "1"
   num_public_agents  = "1"
 
-  dcos_instance_os        = "centos_7.6"
+  dcos_instance_os        = "${local.dcos_instance_os}"
 
-  dcos_variant              = "ee"
+  dcos_variant              = "${local.dcos_variant}"
   dcos_version              = "1.13.0"
   dcos_license_key_contents = "${file("~/license.txt")}"
   ansible_bundled_container = "mesosphere/dcos-ansible-bundle:feature-windows-support-039d79d"
@@ -44,18 +51,24 @@ module "dcos" {
 
 module "winagent" {
   source  = "dcos-terraform/windows-instance/azurerm"
-  version = "~>= 0.0.1"
+  version = "0.0.1"
 
   providers = {
-    aws = "azure"
+    azurerm = "azurerm"
   }
 
-  location = "North Europe"
+  location = "${local.location}"
 
-  dcos_instance_os = "windows_1809"
+  dcos_instance_os = "${local.dcos_winagent_os}"
 
   cluster_name           = "${local.cluster_name}"
   hostname_format        = "%[3]s-winagent%[1]d-%[2]s"
+
+  subnet_id              = "${module.dcos.infrastructure.subnet_id}"
+  resource_group_name    = "${module.dcos.infrastructure.resource_group_name}"
+  vm_size                = "${local.vm_size}"
+  admin_username         = "Administrator"
+  public_ssh_key         = "${local.ssh_public_key_file}"
 
   num = "1"
 }
