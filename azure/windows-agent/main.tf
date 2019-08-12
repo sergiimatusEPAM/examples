af071956aff09f4934a663da1e3cccd99027bb3d
Sergii Matus <sergii_matus@epam.com>
2019-08-12 15:18:38 +0300

    test

--- azure/windows-agent/main.tf
+++ azure/windows-agent/main.tf
@@ -6,13 +6,13 @@ data "http" "whatismyip" {
 }
 
 locals {
-  cluster_name     = "sm-dev1"
-  location         = "North Europe"
+  cluster_name     = "beta"
+  location         = "northeurope"   #"westus"
   dcos_version     = "1.13.0"
   dcos_variant     = "open"
   dcos_instance_os = "centos_7.6"
   dcos_winagent_os = "windows_1809"
-  vm_size          = "Standard_D8s_v3"
+  vm_size          = "Standard_D2s_v3"
   ssh_public_key_file = "~/.ssh/aws-meso-wind.pub"
 }
 
@@ -34,43 +34,59 @@ module "dcos" {
   num_private_agents = "1"
   num_public_agents  = "1"
 
+  bootstrap_vm_size       = "Standard_B2ms"
+  masters_vm_size         = "${local.vm_size}"
+  private_agents_vm_size  = "${local.vm_size}"
+  public_agents_vm_size   = "${local.vm_size}"
+
   dcos_instance_os        = "${local.dcos_instance_os}"
 
   dcos_variant              = "${local.dcos_variant}"
   dcos_version              = "1.13.0"
-  dcos_license_key_contents = "${file("~/license.txt")}"
+  # dcos_license_key_contents = "${file("~/license.txt")}"
   ansible_bundled_container = "mesosphere/dcos-ansible-bundle:feature-windows-support-039d79d"
 
   # provide a SHA512 hashed password, here "deleteme"
   dcos_superuser_password_hash = "$6$rounds=656000$YSvuFmasQDXheddh$TpYlCxNHF6PbsGkjlK99Pwxg7D0mgWJ.y0hE2JKoa61wHx.1wtxTAHVRHfsJU9zzHWDoE08wpdtToHimNR9FJ/"
   dcos_superuser_username      = "demo-super"
 
+  additional_windows_private_agent_os_user   = "${module.winagent.admin_username}"
+  additional_windows_private_agent_passwords = "${module.winagent.windows_passwords}"
   additional_windows_private_agent_ips       = ["${concat(module.winagent.private_ips)}"]
-  #additional_windows_private_agent_passwords = ["${concat(module.winagent.windows_passwords)}"]
 }
 
 module "winagent" {
   source  = "dcos-terraform/windows-instance/azurerm"
-  version = "0.0.1"
 
   providers = {
     azure = "azure"
   }
 
   location = "${local.location}"
-
   dcos_instance_os = "${local.dcos_winagent_os}"
-
   cluster_name           = "${local.cluster_name}"
-  hostname_format        = "%[3]s-winagent%[1]d-%[2]s"
+
+  # Windows hostname limited by 15 characters:
+  hostname_format        = "winagt-%[1]d-%[2]s"
 
   subnet_id              = "${module.dcos.infrastructure.subnet_id}"
   resource_group_name    = "${module.dcos.infrastructure.resource_group_name}"
   vm_size                = "${local.vm_size}"
-  admin_username         = "Administrator"
+  admin_username         = "dcosadmin"
   public_ssh_key         = "${local.ssh_public_key_file}"
 
   num = "1"
+}
+
+output "public_ips" {
+  description = "Windows IP"
+  value = "${module.winagent.public_ips}"
+}
+
+
+output "windows_passwords" {
+  description = "Windows Password for user ${module.winagent.admin_username}"
+  value = "${module.winagent.windows_passwords}"
 }
 
 output "masters_dns_name" {
