7435264af4d4eaff53010998d7aa58f02f9b1ed2
sergiimatusEPAM <49905012+sergiimatusEPAM@users.noreply.github.com>
2019-08-15 12:34:48 +0300

    DEMO52 used example

--- azure/windows-agent/main.tf
+++ azure/windows-agent/main.tf
@@ -6,8 +6,8 @@ data "http" "whatismyip" {
 }
 
 locals {
-  cluster_name     = "beta"
-  location         = "northeurope"   #"westus"
+  cluster_name     = "demo52"
+  location         = "northeurope"
   dcos_version     = "1.13.0"
   dcos_variant     = "open"
   dcos_instance_os = "centos_7.6"
@@ -30,7 +30,7 @@ module "dcos" {
   ssh_public_key_file = "${local.ssh_public_key_file}"
   admin_ips           = ["${data.http.whatismyip.body}/32"]
 
-  num_masters        = "1"
+  num_masters        = "3"
   num_private_agents = "1"
   num_public_agents  = "1"
 
@@ -44,20 +44,20 @@ module "dcos" {
   dcos_variant              = "${local.dcos_variant}"
   dcos_version              = "1.13.0"
   # dcos_license_key_contents = "${file("~/license.txt")}"
-  ansible_bundled_container = "mesosphere/dcos-ansible-bundle:feature-windows-support-039d79d"
+  #ansible_bundled_container = "mesosphere/dcos-ansible-bundle:feature-windows-support-039d79d"
+  ansible_bundled_container = "sergiimatusepam/dcos-ansible-bundle:feature-windows-support"
 
   # provide a SHA512 hashed password, here "deleteme"
   dcos_superuser_password_hash = "$6$rounds=656000$YSvuFmasQDXheddh$TpYlCxNHF6PbsGkjlK99Pwxg7D0mgWJ.y0hE2JKoa61wHx.1wtxTAHVRHfsJU9zzHWDoE08wpdtToHimNR9FJ/"
   dcos_superuser_username      = "demo-super"
 
   additional_windows_private_agent_os_user   = "${module.winagent.admin_username}"
-  additional_windows_private_agent_passwords = "${module.winagent.windows_passwords}"
+  additional_windows_private_agent_passwords = ["${concat(module.winagent.windows_passwords)}"]
   additional_windows_private_agent_ips       = ["${concat(module.winagent.private_ips)}"]
 }
 
 module "winagent" {
-  source  = "dcos-terraform/windows-instance/azurerm"
-
+  source  = "./modules/terraform-azurerm-windows-instance"
   providers = {
     azure = "azure"
   }
@@ -75,7 +75,7 @@ module "winagent" {
   admin_username         = "dcosadmin"
   public_ssh_key         = "${local.ssh_public_key_file}"
 
-  num = "1"
+  num = "3"
 }
 
 output "winagent-ips" {
@@ -85,7 +85,7 @@ output "winagent-ips" {
 
 output "windows_passwords" {
   description = "Windows Password for user ${module.winagent.admin_username}"
-  value = "${module.winagent.windows_passwords}"
+  value = ["${concat(module.winagent.windows_passwords)}"]
 }
 
 output "masters_dns_name" {
